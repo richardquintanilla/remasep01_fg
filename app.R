091381a05d2ca5da15af5607b1a1de5bde4c0b21
richardquintanilla <richard.quintanilla@redsalud.gob.cl>
2026-08-12 10:41:44 -0400

    Update app.R

--- app.R
+++ app.R
@@ -31,43 +31,43 @@ encontrar_archivo <- function(nombres_posibles) {
 rutas_remasep25 <- c(
      "remasep/listados/data/remasep25.fst",
      "../remasep/listados/data/remasep25.fst",
-     "/srv/shiny-server/remasep/listados/data/remasep25.fst"
+     "/srv/shiny-server/data/remasep25.fst"
 )
 
 rutas_f1 <- c(
      "remasep/listados/data/remasep01_f1.fst",
      "../remasep/listados/data/remasep01_f1.fst",
-     "/srv/shiny-server/www/remasep01_f1.fst"
+     "/srv/shiny-server/data/remasep01_f1.fst"
 )
 
 rutas_f2 <- c(
      "remasep/listados/data/remasep01_f2.fst",
      "../remasep/listados/data/remasep01_f2.fst",
-     "/srv/shiny-server/www/remasep01_f2.fst"
+     "/srv/shiny-server/data/remasep01_f2.fst"
 )
 
 rutas_g1 <- c(
      "remasep/listados/data/remasep01_g1.fst",
      "../remasep/listados/data/remasep01_g1.fst",
-     "/srv/shiny-server/www/remasep01_g1.fst"
+     "/srv/shiny-server/data/remasep01_g1.fst"
 )
 
 rutas_g2 <- c(
      "remasep/listados/data/remasep01_g2.fst",
      "../remasep/listados/data/remasep01_g2.fst",
-     "/srv/shiny-server/www/remasep01_g2.fst"
+     "/srv/shiny-server/data/remasep01_g2.fst"
 )
 
 rutas_g3 <- c(
      "remasep/listados/data/remasep01_g3.fst",
      "../remasep/listados/data/remasep01_g3.fst",
-     "/srv/shiny-server/www/remasep01_g3.fst"
+     "/srv/shiny-server/data/remasep01_g3.fst"
 )
 
 rutas_config <- c(
      "remasep/listados/data/config.rds",
      "../remasep/listados/data/config.rds",
-     "/srv/shiny-server/www/config.rds"
+     "/srv/shiny-server/data/config.rds"
 )
 
 # Encontrar rutas válidas
